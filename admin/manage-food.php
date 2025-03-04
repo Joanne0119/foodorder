277240c7554a282dbabeedb00f590978f74c41b0
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-03-04 14:48:46 +0800

    add food function

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -2,41 +2,78 @@
     <div class="main">
         <h1>Manage Food</h1>
         <br/>
-        <a href="#" class="btn-primary">add food</a>
+        <?php
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add']; // Display session message
+                unset($_SESSION['add']); // Remove session message
+            }
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload']; // Display session message
+                unset($_SESSION['upload']); // Remove session message
+            }
+        ?>
+        <br/>
+        <a href="<?php echo SETURL; ?>admin/add-food.php" class="btn-primary">add food</a>
         <table class="tbl-full">
             <tr>
                 <th>S.N.</th>
-                <th>Full Name</th>
-                <th>Username</th>
-                <th>Actions</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Image</th>
+                <th>Feature</th>
+                <th>Active</th>
+                <th>Action</th>
             </tr>
-            <tr>
-                <td>1.</td>
-                <td>Joanne Liu</td>
-                <td>Joeee</td>
-                <td>
-                    <a herf="#" class="btn-secondary">Update Food</a>
-                    <a herf="#" class="btn-danger">Delete Food</a>
-                </td>
-            </tr>
-            <tr>
-                <td>2.</td>
-                <td>Nancy Lee</td>
-                <td>Nana</td>
-                <td>
-                    <a herf="#" class="btn-secondary">Update Food</a>
-                    <a herf="#" class="btn-danger">Delete Food</a>
-                </td>
-            </tr>
-            <tr>
-                <td>3.</td>
-                <td>Peter Pan</td>
-                <td>Baby</td>
-                <td>
-                    <a herf="#" class="btn-secondary">Update Food</a>
-                    <a herf="#" class="btn-danger">Delete Food</a>
-                </td>
-            </tr>
+            <?php
+                $conn = getDbConnection();
+
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT * FROM tbl_food"; //get the data
+                $result = $conn->query($sql);
+
+                $sn = 1;
+
+                if ($result->num_rows > 0) {  
+                    while($row = $result->fetch_assoc()) { //run through the result data, and print it out
+                    ?>
+                        <tr>
+                        <td><?= $sn++; ?></td>
+                        <td><?= $row["title"]; ?></td>
+                        <td>$<?= $row["price"]; ?></td>
+                        <td><?= $row["description"]; ?></td>
+                        <td>
+                            <?php
+                                if($row["image_name"] != ""){
+                                    ?>
+                                    <img src="<?php echo SETURL; ?>image/food/<?php echo $row["image_name"]; ?>" width="150px"> 
+                                    <?php
+                                }
+                                else{
+                                    echo "<div class='fail'>Image Not Added</div>";
+                                }
+                            ?>
+                        </td>
+                        <td><?= $row["feature"]; ?></td>
+                        <td><?= $row["active"]; ?></td>
+                        <td>
+                            <a href="<?php echo SETURL; ?>admin/update-category.php?id=<?php echo $row["id"]; ?>" class="btn-secondary">Update Food</a>
+                            <a href="<?php echo SETURL; ?>admin/delete-category.php?id=<?php echo $row["id"]; ?>&image_name=<?php echo $row["image_name"]; ?>" class="btn-danger">Delete Food</a>
+                        </td>
+                    </tr>
+
+                    <?php
+                    }
+                } else {
+                    echo "No Data";
+                }
+                
+            ?>
         </table>
 
     </div>
